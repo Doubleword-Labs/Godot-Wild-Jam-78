8e59eda732287e30d77d6518e38e190c8ac24d6e
Nathan Dunn <nathanstephendunn@gmail.com>
2025-02-17 22:30:14 +1100

    UI Settings changes Master/Music/SFX

--- globals/audio_player.gd
+++ globals/audio_player.gd
@@ -1,10 +1,23 @@
-extends AudioStreamPlayer
+extends Node
 
 const game_music: AudioStream = preload("res://assets/music/jungle-story-168459.mp3")
 
 var music_player: AudioStreamPlayer
-var volume_music:= 0.0
-var volume_sfx:= 0.0
+var default_volumes = [1, 0.4, 0.6]
+
+
+func _ready():
+	update_volume(0, default_volumes[0])
+	update_volume(1, default_volumes[1])
+	update_volume(2, default_volumes[2])
+
+
+func update_volume(id, value):
+	if value == 0:
+		AudioServer.set_bus_mute(id, true)
+	else:
+		AudioServer.set_bus_mute(id, false)
+		AudioServer.set_bus_volume_db(id, linear_to_db(value))
 
 func play_music():
 	if music_player != null:
@@ -13,13 +26,14 @@ func play_music():
 	music_player = AudioStreamPlayer.new()
 	music_player.stream = game_music
 	music_player.autoplay = true
+	music_player.bus = "Music"
 	add_child(music_player)
 	music_player.play()	
 	
 func play_sfx(sfx: AudioStream):
 	var sfx_player = AudioStreamPlayer.new()
 	sfx_player.stream = sfx
-	sfx_player.volume_db = volume_sfx
+	music_player.bus = "SFX"
 	add_child(sfx_player)
 	sfx_player.play()
 	
--- menus/settings.gd
+++ menus/settings.gd
@@ -2,5 +2,27 @@ extends Node2D
 
 var title_path := "res://menus/title.tscn"
 
+@onready var master_volume = $SoundSettings/Master/MasterVolume
+@onready var music_volume = $SoundSettings/Music/MusicVolume
+@onready var sfx_volume = $SoundSettings/SFX/SFXVolume
+
+
+func _ready() -> void:
+	master_volume.value = AudioPlayer.default_volumes[0]
+	music_volume.value = AudioPlayer.default_volumes[1]
+	sfx_volume.value = AudioPlayer.default_volumes[2]
+
 func _on_return_pressed() -> void:	
 	get_tree().change_scene_to_file(title_path)
+
+
+func _on_master_volume_value_changed(value: float) -> void:
+	AudioPlayer.update_volume(0, value)
+
+
+func _on_music_volume_value_changed(value: float) -> void:
+	AudioPlayer.update_volume(1, value)
+
+
+func _on_sfx_volume_value_changed(value: float) -> void:
+	AudioPlayer.update_volume(2, value)
