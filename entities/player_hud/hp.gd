c18814133f5d10c274cefdfdb6d4c9f0267a1766
Nathan Dunn <nathanstephendunn@gmail.com>
2025-02-18 10:19:34 +1100

    Minor code for naming in HP

--- entities/player_hud/hp.gd
+++ entities/player_hud/hp.gd
@@ -1,7 +1,9 @@
 extends ProgressBar
 class_name Gui_HP
 
+@onready var hp_gui = $"."
+
 func _ready() -> void:
-	$".".max_value = 100
-	$".".value = Game.get_player().health
-	Game.hp_gui = $"."
+	hp_gui.max_value = 100
+	hp_gui.value = Game.get_player().health
+	Game.hp_gui = hp_gui
