b11c4c9d1255871b256a3a555c340ec1b56a5724
Nathan Dunn <nathanstephendunn@gmail.com>
2025-02-18 14:56:07 +1100

    bugfix with paused_gui_node null before ingame

--- globals/game.gd
+++ globals/game.gd
@@ -8,18 +8,19 @@ var paused_gui_node
 var paused = false
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_released("pause"):
-		if (paused):
-			paused = false
-			get_tree().paused = false
-			paused_gui_node.visible = false
-			process_mode = Node.PROCESS_MODE_PAUSABLE
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		else:
-			paused = true
-			get_tree().paused = true
-			paused_gui_node.visible = true
-			process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+	if paused_gui_node != null:
+		if Input.is_action_just_released("pause"):
+			if (paused):
+				paused = false
+				get_tree().paused = false
+				paused_gui_node.visible = false
+				process_mode = Node.PROCESS_MODE_PAUSABLE
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			else:
+				paused = true
+				get_tree().paused = true
+				paused_gui_node.visible = true
+				process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 
 
 func get_player() -> Player:
