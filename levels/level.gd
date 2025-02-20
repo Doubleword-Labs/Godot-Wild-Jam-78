d983d74b321dce4da9ed8b33dfb1f2aae522966b
Nathan Dunn <nathanstephendunn@gmail.com>
2025-02-21 00:01:20 +1100

    Shop Doors for Shop/Exit to go to next level or print to show shop

--- levels/level.gd
+++ levels/level.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if Waves.shop_time:
 		Game.wave_gui.text = "Shop Time!"
+		Game.stationery_gui.text = ""
 	else:
 		Game.wave_gui.text = "Wave: "+str(Waves.current_wave)
 
@@ -17,3 +18,12 @@ func _exit_tree() -> void:
 
 func _on_lava_area_body_entered(body: Node3D) -> void:
 	Game.die()
+
+
+func _on_shop_door_body_entered(body: Node3D) -> void:
+	print("Shop Opened!") # Replace with function body.
+
+
+func _on_exit_door_body_entered(body: Node3D) -> void:
+	Waves.shop_time = false
+	Waves.set_level()
