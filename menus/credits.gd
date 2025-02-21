ad4fa2bd4cb1d024ba6bc8af12387db22484697d
Nathan Dunn <nathanstephendunn@gmail.com>
2025-02-21 19:00:01 +1100

    Scrollable credits so there is more space now

--- menus/credits.gd
+++ menus/credits.gd
@@ -2,5 +2,23 @@ extends Node2D
 
 var title_path := "res://menus/title.tscn"
 
+@onready var scroll: ScrollContainer = $ScrollContainer
+
+var scroll_down = true
 func _on_return_pressed() -> void:	
 	get_tree().change_scene_to_file(title_path)
+
+
+func _on_timer_timeout() -> void:
+	if scroll.scroll_vertical == 0:
+		scroll_down = true
+
+	if scroll.scroll_vertical == 150:
+		scroll_down = false
+
+	if scroll_down:
+		scroll.scroll_vertical += 1
+	else:
+		scroll.scroll_vertical -= 1
+	
+	print(scroll.scroll_vertical)
