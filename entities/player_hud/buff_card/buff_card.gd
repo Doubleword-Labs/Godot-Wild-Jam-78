916f538928b71245867707a927a442ac8df1d28b
Nathan Dunn <nathanstephendunn@gmail.com>
2025-02-22 18:17:38 +1100

    Randomly allow 3 items for shop with Buff Cards

--- entities/player_hud/buff_card/buff_card.gd
+++ entities/player_hud/buff_card/buff_card.gd
@@ -1,16 +1,33 @@
 extends Node
 class_name BuffCard
 
-@export var buffs:Array[BuffResource] = []
-
 var internal_name: String = "internal"
-
-func render_buff(resource: BuffResource):
-	internal_name = resource.internal_name
-	$Label.text = resource.headline
-	$TextureRect.texture = resource.texture
-	$Button.text = resource.text
 
 
 func _on_purchase_pressed() -> void:
 	print(internal_name)
+	
+	if internal_name == "ogre":
+		Buff.player_ogre = true
+				
+	if internal_name == "flash":
+		Buff.player_flash = true
+		
+	if internal_name == "vampire":
+		Buff.player_vampire = true
+		
+	if internal_name == "regen":
+		Buff.player_regen = true
+		
+	if internal_name == "damage":
+		Buff.player_damage = true
+		
+	if internal_name == "minigun":
+		Buff.weapon_minigun = true
+		Game.get_player()._request_weapon(1)
+		
+	if internal_name == "stapler":
+		Buff.weapon_staple = true
+		Game.get_player()._request_weapon(2)
+		
+	Waves.exit_shop()
--- entities/player_hud/player_hud_shop_random_buff.gd
+++ entities/player_hud/player_hud_shop_random_buff.gd
@@ -1,1 +1,24 @@
 extends CanvasLayer
+
+var BUFF_CARD = preload("res://entities/player_hud/buff_card/buff_card.tscn")
+
+@onready var container: HBoxContainer = $HBoxContainer
+
+@export var buffs:Array[BuffResource] = []
+
+@onready var buffcard = $BuffCard
+@onready var buffcard2 = $BuffCard2
+@onready var buffcard3 = $BuffCard3
+
+func _ready() -> void:
+	render_buff(buffcard,buffs.pick_random())
+	render_buff(buffcard2,buffs.pick_random())
+	render_buff(buffcard3,buffs.pick_random())
+
+
+func  render_buff(node: BuffCard, resource: BuffResource):
+	node.internal_name = resource.internal_name
+	node.get_node("Headline").text = resource.headline
+	node.get_node("TextureRect").texture = resource.texture
+	node.get_node("Subtitle").text = resource.text
+	
