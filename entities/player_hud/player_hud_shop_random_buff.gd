97e90f6ce2621f298caa84c097f5b9f6cbf8e540
Nathan Dunn <nathanstephendunn@gmail.com>
2025-02-22 18:33:41 +1100

    show 3 discrete random buffs

--- entities/player_hud/player_hud_shop_random_buff.gd
+++ entities/player_hud/player_hud_shop_random_buff.gd
@@ -5,16 +5,33 @@ var BUFF_CARD = preload("res://entities/player_hud/buff_card/buff_card.tscn")
 @onready var container: HBoxContainer = $HBoxContainer
 
 @export var buffs:Array[BuffResource] = []
+var buffs2:Array[BuffResource]
 
 @onready var buffcard = $BuffCard
 @onready var buffcard2 = $BuffCard2
 @onready var buffcard3 = $BuffCard3
 
 func _ready() -> void:
-	render_buff(buffcard,buffs.pick_random())
-	render_buff(buffcard2,buffs.pick_random())
-	render_buff(buffcard3,buffs.pick_random())
+	#show 3 discrete random buffs
+	var buff = buffs.pick_random()
+	render_buff(buffcard, buff)
+	remove_buff(buff)
+	
+	buff = buffs.pick_random()
+	render_buff(buffcard2, buff)
+	remove_buff(buff)
+	
+	buff = buffs.pick_random()
+	render_buff(buffcard3, buff)
 
+
+func  remove_buff(buff: BuffResource):
+	var temp: Array[BuffResource] = []
+	for b in buffs:
+		if buff.internal_name != b.internal_name:
+			temp.append(b)
+	buffs = temp
+	
 
 func  render_buff(node: BuffCard, resource: BuffResource):
 	node.internal_name = resource.internal_name
