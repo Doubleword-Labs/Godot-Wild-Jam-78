1a4bf23974d0cdfc881928bbfe4cd0a2cc95c17e
Simon Hartcher <simon@simonhartcher.com>
2025-02-23 15:23:31 +1100

    feat: add ambience to shop

--- entities/tiki_hut/tiki_hut.gd
+++ entities/tiki_hut/tiki_hut.gd
@@ -2,6 +2,7 @@
 extends Node3D
 
 @onready var sign_label_3d: Label3D = %SignLabel3D
+@onready var ambient_source: Node3D = $"Ambient-Source"
 
 @export var sign_text := "Shop":
 	set(value):
@@ -13,3 +14,7 @@ extends Node3D
 func _ready() -> void:
 	if is_instance_valid(sign_label_3d):
 		sign_label_3d.text = sign_text
+
+	if is_instance_valid(ambient_source) and is_instance_valid(ambient_source.audio_player_node):
+		ambient_source.audio_player_node.bus = "SFX"
+		ambient_source.player_node = Game.get_player()
