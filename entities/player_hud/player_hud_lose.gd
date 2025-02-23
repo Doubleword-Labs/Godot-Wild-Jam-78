97d72a44ea565cb0576d517e6c27a61013b590d2
Simon Hartcher <simon@simonhartcher.com>
2025-02-24 01:22:27 +1100

    fix: player hud win/lose

--- entities/player_hud/player_hud_lose.gd
+++ entities/player_hud/player_hud_lose.gd
@@ -3,17 +3,15 @@ extends CanvasLayer
 var title_path = "res://menus/title.tscn"
 
 
-func _ready() -> void:	
+func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 
 
 func _on_wave_again_pressed() -> void:
 	Game.reload()
 	get_tree().reload_current_scene()
-	#get_tree().change_scene_to_file(Game.current_level)
 
 
 func _on_title_pressed() -> void:
 	Waves.current_wave = 1
-	Game.reload()
 	get_tree().change_scene_to_file(title_path)
--- entities/player_hud/player_hud_win.gd
+++ entities/player_hud/player_hud_win.gd
@@ -12,19 +12,15 @@ func _on_wave_again_pressed() -> void:
 	Game.reload()
 	if (Waves.current_wave - 1) % 4 == 3:
 		Waves.shop_time = true
-		
+
 	Waves.current_wave += 1
-	
+
 	if Waves.shop_time:
 		Waves.set_shop()
 	else:
 		Waves.set_level()
-		
-		
-	
 
 
 func _on_title_pressed() -> void:
 	Waves.current_wave = 1
-	Game.reload()
 	get_tree().change_scene_to_file(title_path)
